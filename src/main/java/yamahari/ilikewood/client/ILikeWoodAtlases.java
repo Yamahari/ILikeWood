cf2598063e83e9f8da1b477570d9fce603b9548a
Yamahari <40495288+yamahari@users.noreply.github.com>
2020-02-08 22:01:07 +0100

    cleaned up atlas
    Signed-off-by: Yamahari <40495288+Yamahari@users.noreply.github.com>

--- src/main/java/yamahari/ilikewood/client/ILikeWoodAtlases.java
+++ src/main/java/yamahari/ilikewood/client/ILikeWoodAtlases.java
@@ -10,48 +10,44 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import yamahari.ilikewood.util.Constants;
 import yamahari.ilikewood.util.Util;
-import yamahari.ilikewood.util.WoodType;
 import yamahari.ilikewood.util.WoodTypes;
 
+import java.util.EnumMap;
 import java.util.Map;
-import java.util.stream.Stream;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public final class ILikeWoodAtlases {
-    private static final Map<String, Material> LEFT_CHESTS = makeChestMaterials(ChestType.LEFT);
-    private static final Map<String, Material> RIGHT_CHESTS = makeChestMaterials(ChestType.RIGHT);
-    private static final Map<String, Material> SINGLE_CHESTS = makeChestMaterials(ChestType.SINGLE);
+    private static final Map<String, Map<ChestType, Material>> CHESTS;
+
+    static {
+        final ImmutableMap.Builder<String, Map<ChestType, Material>> builder = new ImmutableMap.Builder<>();
+        WoodTypes.get().map(Object::toString).forEach(woodType -> builder.put(woodType, ImmutableMap.copyOf(makeChestMaterials(woodType))));
+        CHESTS = builder.build();
+    }
 
     private ILikeWoodAtlases() {
     }
 
-    private static Map<String, Material> makeChestMaterials(final ChestType chestType) {
-        final ImmutableMap.Builder<String, Material> builder = new ImmutableMap.Builder<>();
-        WoodTypes.get().map(WoodType::toString)
-                .forEach(s -> builder.put(s,
-                        new Material(Atlases.field_228747_f_,
-                                new ResourceLocation(Constants.MOD_ID, Util.toPath("entity", "chest", chestType.getName(), s)))));
-        return builder.build();
+    private static Map<ChestType, Material> makeChestMaterials(final String woodType) {
+        final EnumMap<ChestType, Material> materials = new EnumMap<>(ChestType.class);
+        for (final ChestType chestType : ChestType.values()) {
+            materials.put(chestType, new Material(Atlases.field_228747_f_,
+                    new ResourceLocation(Constants.MOD_ID, Util.toPath("entity", "chest", chestType.getName(), woodType))));
+        }
+        return materials;
     }
 
-    public static Material getChestMaterial(final String type, final ChestType chestType) {
-        switch (chestType) {
-            default:
-            case SINGLE:
-                return SINGLE_CHESTS.get(type);
-            case LEFT:
-                return LEFT_CHESTS.get(type);
-            case RIGHT:
-                return RIGHT_CHESTS.get(type);
-        }
+    public static Map<ChestType, Material> getChestMaterials(final String type) {
+        return CHESTS.get(type);
     }
 
     @SubscribeEvent
     public static void onTextureStitchPre(final TextureStitchEvent.Pre event) {
         final ResourceLocation atlas = event.getMap().func_229223_g_();
-        Stream.of(SINGLE_CHESTS, LEFT_CHESTS, RIGHT_CHESTS)
-                .flatMap(map -> map.values().stream())
+        CHESTS.values().stream()
+                .flatMap(materials -> materials.values().stream())
                 .filter(material -> material.func_229310_a_().equals(atlas))
                 .forEach(material -> event.addSprite(material.func_229313_b_()));
+
     }
 }
--- src/main/java/yamahari/ilikewood/client/tileentity/renderer/WoodenChestTileEntityRenderer.java
+++ src/main/java/yamahari/ilikewood/client/tileentity/renderer/WoodenChestTileEntityRenderer.java
@@ -8,13 +8,22 @@ import net.minecraft.tileentity.TileEntity;
 import yamahari.ilikewood.client.ILikeWoodAtlases;
 import yamahari.ilikewood.client.tileentity.WoodenChestTileEntity;
 
+import java.util.Map;
+
 public final class WoodenChestTileEntityRenderer extends ChestTileEntityRenderer {
+    private Map<ChestType, Material> materials;
+
     public WoodenChestTileEntityRenderer(final TileEntityRendererDispatcher dispatcher) {
         super(dispatcher);
+        this.materials = null;
     }
 
     @Override
     protected Material getMaterial(final TileEntity tileEntity, final ChestType chestType) {
-        return ILikeWoodAtlases.getChestMaterial(((WoodenChestTileEntity) tileEntity).getWoodType().toString(), chestType);
+        if (this.materials == null) {
+            this.materials = ILikeWoodAtlases.getChestMaterials(((WoodenChestTileEntity) tileEntity).getWoodType().toString());
+        }
+        assert this.materials != null;
+        return this.materials.get(chestType);
     }
 }
