1982486de9be47dd031d22dfe2de84139eb9664f
Yamahari <40495288+yamahari@users.noreply.github.com>
2020-03-30 19:14:40 +0200

    jei plugin
    Signed-off-by: Yamahari <40495288+Yamahari@users.noreply.github.com>

---
+++ src/main/java/yamahari/ilikewood/compat/jei/ILikeWoodPlugin.java
@@ -0,0 +1,40 @@
+package yamahari.ilikewood.compat.jei;
+
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.VanillaRecipeCategoryUid;
+import mezz.jei.api.registration.IGuiHandlerRegistration;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
+import mezz.jei.api.registration.IRecipeTransferRegistration;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import yamahari.ilikewood.container.WoodenWorkBenchContainer;
+import yamahari.ilikewood.registry.WoodenBlocks;
+import yamahari.ilikewood.util.Constants;
+import yamahari.ilikewood.util.WoodenObjectType;
+
+@JeiPlugin
+public final class ILikeWoodPlugin implements IModPlugin {
+    private static final ResourceLocation PLUGIN_UID = new ResourceLocation(Constants.MOD_ID, "plugin");
+
+    @Override
+    public ResourceLocation getPluginUid() {
+        return PLUGIN_UID;
+    }
+
+    @Override
+    public void registerRecipeTransferHandlers(final IRecipeTransferRegistration registration) {
+        registration.addRecipeTransferHandler(WoodenWorkBenchContainer.class, VanillaRecipeCategoryUid.CRAFTING, 1, 9, 10, 36);
+    }
+
+    @Override
+    public void registerRecipeCatalysts(final IRecipeCatalystRegistration registration) {
+        WoodenBlocks.getBlocks(WoodenObjectType.CRAFTING_TABLE)
+                .forEach(block -> registration.addRecipeCatalyst(new ItemStack(block), VanillaRecipeCategoryUid.CRAFTING));
+    }
+
+    @Override
+    public void registerGuiHandlers(final IGuiHandlerRegistration registration) {
+
+    }
+}
