6b1947e8de303a028fd67720fd0245e1415ab7f1
Yamahari <40495288+yamahari@users.noreply.github.com>
2022-07-28 14:55:03 +0200

    fixed issue with item frames cause by new registry names
    Signed-off-by: Yamahari <40495288+Yamahari@users.noreply.github.com>

--- main/src/main/java/yamahari/ilikewood/provider/itemmodel/ItemFrameItemModelProvider.java
+++ main/src/main/java/yamahari/ilikewood/provider/itemmodel/ItemFrameItemModelProvider.java
@@ -4,23 +4,31 @@ import net.minecraft.data.DataGenerator;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.ForgeRegistries;
 import yamahari.ilikewood.registry.objecttype.WoodenItemType;
 import yamahari.ilikewood.registry.woodtype.IWoodType;
 import yamahari.ilikewood.util.IWooden;
 import yamahari.ilikewood.util.Util;
 
-public final class ItemFrameItemModelProvider extends AbstractItemModelProvider {
-    public ItemFrameItemModelProvider(final DataGenerator generator, final ExistingFileHelper helper) {
+public final class ItemFrameItemModelProvider
+    extends AbstractItemModelProvider
+{
+    public ItemFrameItemModelProvider(
+        final DataGenerator generator,
+        final ExistingFileHelper helper
+    )
+    {
         super(generator, helper, WoodenItemType.ITEM_FRAME);
     }
 
     @Override
-    protected void registerModel(final Item item) {
+    protected void registerModel(final Item item)
+    {
         final IWoodType woodType = ((IWooden) item).getWoodType();
+
         this
-            .getBuilder(Util.toRegistryName(woodType.getName(), WoodenItemType.ITEM_FRAME.getName()))
+            .getBuilder(ForgeRegistries.ITEMS.getKey(item).getPath())
             .parent(new ModelFile.UncheckedModelFile(mcLoc(Util.toPath(ITEM_FOLDER, "generated"))))
-            .texture("layer0",
-                modLoc(Util.toPath(ITEM_FOLDER, WoodenItemType.ITEM_FRAME.getName(), woodType.getName())));
+            .texture("layer0", modLoc(Util.toPath(ITEM_FOLDER, WoodenItemType.ITEM_FRAME.getName(), woodType.getName())));
     }
 }
