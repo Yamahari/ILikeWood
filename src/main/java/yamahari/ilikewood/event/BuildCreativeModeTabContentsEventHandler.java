4c31031d3be144f2e9b68c543acdadbd401bf0bb
Yamahari <40495288+Yamahari@users.noreply.github.com>
2023-09-07 19:23:51 +0200

    fix crash when opening creative menu when objects are disabled via config file

--- src/main/java/yamahari/ilikewood/event/BuildCreativeModeTabContentsEventHandler.java
+++ src/main/java/yamahari/ilikewood/event/BuildCreativeModeTabContentsEventHandler.java
@@ -6,9 +6,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import yamahari.ilikewood.ILikeWood;
 import yamahari.ilikewood.registry.ILikeWoodCreativeModeTabs;
-import yamahari.ilikewood.registry.objecttype.WoodenBlockType;
-import yamahari.ilikewood.registry.objecttype.WoodenItemType;
-import yamahari.ilikewood.registry.objecttype.WoodenTieredItemType;
+import yamahari.ilikewood.registry.objecttype.*;
 import yamahari.ilikewood.util.Constants;
 
 @Mod.EventBusSubscriber(value = {Dist.CLIENT, Dist.DEDICATED_SERVER}, modid = Constants.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
@@ -16,9 +14,11 @@ public final class BuildCreativeModeTabContentsEventHandler {
     @SubscribeEvent
     public static void onBuildCreativeModeTabContents(final BuildCreativeModeTabContentsEvent event) {
         if (event.getTabKey() == ILikeWoodCreativeModeTabs.CREATIVE_MODE_TAB.getKey()) {
-            ILikeWood.BLOCK_REGISTRY.getObjects(WoodenBlockType.getAll()).forEach(event::accept);
-            ILikeWood.ITEM_REGISTRY.getObjects(WoodenItemType.getAll()).forEach(event::accept);
-            WoodenTieredItemType.getAll().forEach(woodenTieredItemType -> ILikeWood.TIERED_ITEM_REGISTRY.getObjects(woodenTieredItemType).forEach(event::accept));
+            ILikeWood.BLOCK_REGISTRY.getObjects(WoodenBlockType.getAll().filter(AbstractWoodenObjectType::isEnabled)).forEach(event::accept);
+            ILikeWood.ITEM_REGISTRY.getObjects(WoodenItemType.getAll().filter(AbstractWoodenObjectType::isEnabled)).forEach(event::accept);
+            WoodenTieredItemType.getAll()
+                .filter(AbstractWoodenTieredObjectType::isEnabled)
+                .forEach(woodenTieredItemType -> ILikeWood.TIERED_ITEM_REGISTRY.getObjects(woodenTieredItemType).forEach(event::accept));
         }
     }
 }
